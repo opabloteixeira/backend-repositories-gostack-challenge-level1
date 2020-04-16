b378a211052a3d07511b3aefd262eb9aa8a8c675
Pablo Teixeira <pablopst@gmail.com>
2020-04-16 18:10:26 -0300

    finished

---
+++ README.md
@@ -0,0 +1,17 @@
+# API Node.js
+## (CRUD and like) challenge module2 gostack
+
+## Dependencies
+
+```
+  "dependencies": {
+    "cors": "^2.8.5",
+    "express": "^4.17.1",
+    "uuidv4": "^6.0.7"
+  },
+  "devDependencies": {
+    "jest": "^25.2.6",
+    "nodemon": "^2.0.2",
+    "supertest": "^4.0.2"
+  }
+```
---
+++ src/app.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const cors = require("cors");
+
+const { uuid } = require("uuidv4");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const repositories = [];
+
+app.get("/repositories", (request, response) => {
+  return response.json(repositories);
+});
+
+app.post("/repositories", (request, response) => {
+  const { title, url, techs } = request.body;
+  const id = uuid();
+  const newProject = {id, title, url, techs, likes: 0 }
+  repositories.push(newProject);
+
+  return response.json(newProject);
+});
+
+
+app.put("/repositories/:id", (request, response) => {
+
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repositoryIndex = repositories.findIndex( repo => repo.id === id );
+  const { likes } = repositories.find( repo => repo.id === id );
+
+  if(repositoryIndex < 0){
+    return response.status(400).json({ error: "Project not found." })
+  }
+
+  const newRepository = {id, title, url, techs, likes};
+
+  repositories[repositoryIndex] = newRepository;
+
+  return response.json(newRepository);
+
+});
+
+
+app.delete("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex( repo => repo.id === id );
+
+  if(repositoryIndex < 0){
+    return response.status(400).json({ error: "Project not found." });
+  }
+
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).send();
+});
+
+
+
+app.post("/repositories/:id/like", (request, response) => {
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(repo => repo.id === id);
+  const repository = repositories.find(repo => repo.id === id);
+
+  if(repositoryIndex < 0){
+    return response.status(400).json({ error: "Project not found." });
+  }
+
+  repository.likes = repository.likes + 1;
+
+  repositories[repositoryIndex] = repository;
+
+  return response.json(repository);
+});
+
+
+module.exports = app;
